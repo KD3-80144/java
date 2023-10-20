505808340fe6c9d93cb2131e8875c6563fccb9f8
74546-shubham <smbansode03@gmail.com>
2023-10-20 23:31:10 +0530

    Day14_Q6

---
+++ Day14_Q6/src/com/sunbeam/FactorialNumber.java
@@ -0,0 +1,16 @@
+package com.sunbeam;
+
+import java.util.stream.Stream;
+
+public class FactorialNumber {
+
+	public static void main(String[] args) 
+	{
+		int num =6;
+		Stream<Integer> s =Stream.iterate(1,i->i+1).limit(num);
+		int result = s.reduce(1,(a,e)->a*e);
+		System.out.println("RESULT = "+result);
+
+	}
+
+}
