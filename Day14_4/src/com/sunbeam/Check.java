96f464cf86e5413d2dcd718ff67f163f5bf0a18e
74546-shubham <smbansode03@gmail.com>
2023-10-20 23:30:38 +0530

    Day14_4

---
+++ Day14_4/src/com/sunbeam/Check.java
@@ -0,0 +1,33 @@
+package com.sunbeam;
+@FunctionalInterface
+
+public interface Check<T> 
+{
+	boolean compare(T a, T b);
+	
+
+
+   static<T> int countIf(T[]arr,T key, Check <T> c)
+   {
+     int count =0;
+     for (T ele: arr)
+     {
+    	 if(c.compare (ele,key))
+    		 count++;
+      }
+     return count;
+   }
+   public static void main(String[] args) 
+	{
+		Integer [] arr = {44, 77, 99, 22, 55, 66};
+		Integer key = 50;
+		int cnt = countIf(arr, key, (x,y)-> x > y);
+		System.out.println("Count = " + cnt);
+	}
+
+ }
+   
+   
+
+
+   
